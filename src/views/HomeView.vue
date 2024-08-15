16bc7f570be36d1edcf5566c5842ee2941301756
SpigotWorkspace <florian.rief@outlook.de>
2024-08-15 23:14:55 +0200

    working on join game button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const dialogRef = ref<HTMLDialogElement | undefined>(undefined)
+const gameIdInputRef = ref<HTMLInputElement | undefined>(undefined)
+
 function createGame() {
   console.debug('createGame')
 }
 
 function joinGame() {
-  dialogRef.value?.showModal()
+  console.debug('gameId', gameIdInputRef.value?.value)
 }
-
-const dialogRef = ref<HTMLDialogElement | undefined>(undefined)
 </script>
 
 <template>
   <main>
     <button class="button" @click="createGame">Create Game</button>
-    <button class="button" @click="joinGame">Join Game</button>
-
-    <dialog ref="dialogRef">
-      <button autofocus @click="dialogRef?.close">Close</button>
-    </dialog>
+    <button class="button" @click="dialogRef?.showModal()">Join Game</button>
   </main>
+
+  <dialog ref="dialogRef">
+    <div
+      style="display: flex; justify-content: center; align-items: center; flex-direction: column"
+    >
+      <div style="display: grid; gap: 0.2rem">
+        <label for="input-game-id" style="font-weight: bold; color: rgb(0, 0, 0)">Game-ID</label>
+        <input type="text" id="input-game-id" ref="gameIdInputRef" />
+      </div>
+      <br />
+      <button class="button" @click="joinGame">Join Game</button>
+      <br />
+      <button class="button" autofocus @click="dialogRef?.close">Close</button>
+    </div>
+  </dialog>
 </template>
 
 <style scoped>
@@ -42,11 +54,32 @@ main {
   min-height: 30px;
   width: 20vw;
   height: 5vh;
+  font-size: 0.9rem;
+}
+
+#input-game-id {
+  border: none;
+  border-radius: 0.3rem;
+  background-color: #b6b6cc;
+  height: 3vh;
+  width: 19vw;
+  font-size: 0.9rem;
+  padding: 0.2rem;
+}
+
+#input-game-id:focus {
+  outline: none;
 }
 
 dialog {
-  align-self: center;
-  height: 10vh;
-  width: 10vw;
+  background: linear-gradient(#6482e6, #9198e5, #8989c9);
+  border: none;
+  border-radius: 1rem;
+  overflow: hidden;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 25vw;
+  height: 20vh;
 }
 </style>
